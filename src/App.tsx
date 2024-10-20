582405aaf938510351bf4a1edb9d51bda3962758
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-10-20 18:49:17 +0100

    Reveal sketch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { ReactP5Wrapper } from '@p5-wrapper/react';
-import { sketch } from './sketch';
+import { attractOrRepel } from './attractOrRepel';
+import { reveal } from './reveal';
+
+const possibleSketches = [attractOrRepel, reveal];
 
 const App = () => {
+  const sketch =
+    possibleSketches[Math.floor(Math.random() * possibleSketches.length)];
   return <ReactP5Wrapper sketch={sketch} />;
 };
 
